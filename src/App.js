7cc50bf40246ac068cc963ead7427136b277b29a
현태룡 <xofyd99@naver.com>
2019-11-19 20:40:55 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
+      // console.log(movie)
+      return <Movie 
+        title={movie.title} 
+        poster={movie.medium_cover_image} 
+        genres={movie.genres} 
+        synopsis={movie.synopsis}
+        key={movie.id} />
     })
     return movies;
   }
